46e0943c2c9b6448424958fb519f7086e8653217
Sonar Systems <sonarsystems@frahaans-imac.home>
2017-02-01 15:25:18 +0000

    Xcode SFML and Box2D Templates

---
+++ Templates/Xcode/SFML Tut Box2D/SFML Tut/main.cpp
@@ -0,0 +1,39 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+
+int main( )
+{
+    sf::RenderWindow window( sf::VideoMode( 640, 480 ), "Awesome Game" );
+    
+    
+    while ( window.isOpen( ) )
+    {
+        sf::Event event;
+        
+        // handle all events
+        while ( window.pollEvent( event ) )
+        {
+            switch ( event.type )
+            {
+                case sf::Event::Closed:
+                    window.close( );
+                    
+                    break;
+            }
+        }
+        
+        // update the game
+        
+        window.clear( );
+        
+        // draw objects here
+        
+        window.display( );
+    }
+}
+
+
+
+
+
+
