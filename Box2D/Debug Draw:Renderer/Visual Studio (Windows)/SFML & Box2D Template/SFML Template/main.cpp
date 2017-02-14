9ade412b86e2423f7f6fd0d06e73c8d4cdad0070
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-02-14 09:58:54 +0000

    Update main.cpp

--- Box2D/Debug Draw:Renderer/Visual Studio (Windows)/SFML & Box2D Template/SFML Template/main.cpp
+++ Box2D/Debug Draw:Renderer/Visual Studio (Windows)/SFML & Box2D Template/SFML Template/main.cpp
@@ -1,5 +1,5 @@
-#include <SFML\Graphics.hpp>
-#include <Box2D\Box2D.h>
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
 #include "b2GLDraw.h"
 
 /** We need this to easily convert between pixel and real-world coordinates*/
