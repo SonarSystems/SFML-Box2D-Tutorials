f5c1f9b02d28f920202cc1e91aaee120c0025e98
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-08-31 00:23:48 +0100

    SFML and Box2D Cross Platform

---
+++ Templates/Cross Platform/Mac and Windows/SFML & Box2D Cross Platform/SFML Cross Platform/Code/main.cpp
@@ -0,0 +1,130 @@
+#include <SFML/Graphics.hpp>
+#include <Box2D/Box2D.h>
+#include "b2GLDraw.h"
+
+/** We need this to easily convert between pixel and real-world coordinates*/
+static const float SCALE = 30.f;
+
+/** Create the base for the boxes to land */
+void CreateGround(b2World& World, float X, float Y);
+
+/** Create the boxes */
+void CreateBox(b2World& World, int MouseX, int MouseY);
+
+int main()
+{
+    /** Prepare the window */
+    sf::RenderWindow Window(sf::VideoMode(800, 600, 32), "Test");
+    Window.setFramerateLimit(60);
+    
+    /** Prepare the world */
+    b2Vec2 Gravity(0.f, 9.8f);
+    b2World World(Gravity);
+    CreateGround(World, 400.f, 500.f);
+    
+    b2GLDraw fooDrawInstance;
+    World.SetDebugDraw(&fooDrawInstance);
+    uint32 flags = 0;
+    flags += b2Draw::e_shapeBit;
+    flags += b2Draw::e_jointBit;
+    flags += b2Draw::e_aabbBit;
+    flags += b2Draw::e_pairBit;
+    flags += b2Draw::e_centerOfMassBit;
+    fooDrawInstance.SetFlags(flags);
+    
+    /** Prepare textures */
+    sf::Texture GroundTexture;
+    sf::Texture BoxTexture;
+    GroundTexture.loadFromFile("Resources/ground.png");
+    BoxTexture.loadFromFile("Resources/box.png");
+    
+    while (Window.isOpen())
+    {
+        sf::Event event;
+        
+        // handle all events
+        while ( Window.pollEvent( event ) )
+        {
+            switch ( event.type )
+            {
+                case sf::Event::Closed:
+                    Window.close( );
+                    
+                    break;
+            }
+        }
+
+        
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            int MouseX = sf::Mouse::getPosition(Window).x;
+            int MouseY = sf::Mouse::getPosition(Window).y;
+            CreateBox(World, MouseX, MouseY);
+        }
+        World.Step(1 / 60.f, 8, 3);
+        
+        Window.clear(sf::Color::White);
+        
+        
+        int BodyCount = 0;
+        for (b2Body* BodyIterator = World.GetBodyList(); BodyIterator != 0; BodyIterator = BodyIterator->GetNext())
+        {
+            if (BodyIterator->GetType() == b2_dynamicBody)
+            {
+                sf::Sprite Sprite;
+                Sprite.setTexture(BoxTexture);
+                Sprite.setOrigin(16.f, 16.f);
+                Sprite.setPosition(SCALE * BodyIterator->GetPosition().x, SCALE * BodyIterator->GetPosition().y);
+                Sprite.setRotation(BodyIterator->GetAngle() * 180 / b2_pi);
+                Window.draw(Sprite);
+                ++BodyCount;
+            }
+            else
+            {
+                sf::Sprite GroundSprite;
+                GroundSprite.setTexture(GroundTexture);
+                GroundSprite.setOrigin(400.f, 8.f);
+                GroundSprite.setPosition(BodyIterator->GetPosition().x * SCALE, BodyIterator->GetPosition().y * SCALE);
+                GroundSprite.setRotation(180 / b2_pi * BodyIterator->GetAngle());
+                Window.draw(GroundSprite);
+            }
+        }
+        
+        World.DrawDebugData();
+        
+        Window.display();
+    }
+    
+    return 0;
+}
+
+void CreateBox(b2World& World, int MouseX, int MouseY)
+{
+    b2BodyDef BodyDef;
+    BodyDef.position = b2Vec2(MouseX / SCALE, MouseY / SCALE);
+    BodyDef.type = b2_dynamicBody;
+    b2Body* Body = World.CreateBody(&BodyDef);
+    
+    b2PolygonShape Shape;
+    Shape.SetAsBox((32.f / 2) / SCALE, (32.f / 2) / SCALE);
+    b2FixtureDef FixtureDef;
+    FixtureDef.density = 1.f;
+    FixtureDef.friction = 0.7f;
+    FixtureDef.shape = &Shape;
+    Body->CreateFixture(&FixtureDef);
+}
+
+void CreateGround(b2World& World, float X, float Y)
+{
+    b2BodyDef BodyDef;
+    BodyDef.position = b2Vec2(X / SCALE, Y / SCALE);
+    BodyDef.type = b2_staticBody;
+    b2Body* Body = World.CreateBody(&BodyDef);
+    
+    b2PolygonShape Shape;
+    Shape.SetAsBox((800.f / 2) / SCALE, (16.f / 2) / SCALE);
+    b2FixtureDef FixtureDef;
+    FixtureDef.density = 0.f;
+    FixtureDef.shape = &Shape;
+    Body->CreateFixture(&FixtureDef);
+}
